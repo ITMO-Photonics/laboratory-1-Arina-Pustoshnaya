199d55810226d6c695c2c34a2f9dda6403dd1219
Arina <elihandro@yandex.ru>
2018-03-31 14:42:36 +0300

    Done

--- task3.py
+++ task3.py
@@ -1,15 +1,12 @@
 import numpy as np
 import scipy.linalg as linalg
 
-N = 100
-A = np.zeros((N,N))
-
-i,j = np.indices((N,N))
-A[i==j] = 1
-A[i==j+1] = 1
-A[i==j+2] = 1
-b=np.arange(N)
-
-
-B=np.linalg.solve(A, b)
-print(B);
+n=100
+A=np.zeros((n,n))
+i,j=np.indices((n,n))
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+B=np.arange(n)
+f=linalg.solve(A,B)
+print(f)
